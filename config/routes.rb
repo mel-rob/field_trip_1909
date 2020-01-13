dd0e547d2d448dc48ee95f96e78a063ce9e5f3d3
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-13 09:18:59 -0700

    Refactor routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/airlines', to: 'airlines#index'
-  get '/airlines/:id', to: 'airlines#show'
+  resources :airlines, only: [:index, :show]
 end
