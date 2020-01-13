47de088f253aa85f499c159edf536fa732808e61
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-13 11:08:26 -0700

    Add tests to add a flight

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Passengers Show Page" do
 
     @flight_1 = @southwest.flights.create!(number: "123", date: "01/11/20", time: "13:00", departure_city: "Denver, CO", arrival_city: "San Francisco, CA")
     @flight_2 = @southwest.flights.create!(number: "456", date: "01/14/20", time: "16:25", departure_city: "San Francisco, CA", arrival_city: "Denver, CO")
+    @flight_3 = @southwest.flights.create!(number: "789", date: "01/20/20", time: "16:12", departure_city: "Denver, CO", arrival_city: "Honolulu, HI")
 
     @passenger_1 = Passenger.create!(name: "Melissa", age: 34)
 
@@ -25,6 +26,24 @@ RSpec.describe "Passengers Show Page" do
       click_link(@flight_1.number)
 
       expect(current_path).to eq(flight_path(@flight_1))
+    end
+
+    it 'I see a form to add a flight' do
+
+      visit passenger_path(@passenger_1)
+
+      expect(page).to_not have_link('789')
+
+      expect(page).to have_content('Add Flight:')
+
+      fill_in :flight_number, with: '789'
+      click_button 'Submit'
+      save_and_open_page
+
+      expect(page).to have_content('Flight added!')
+      expect(current_path).to eq(passenger_path(@passenger_1))
+      expect(page).to have_link('789')
+
     end
   end
 end
